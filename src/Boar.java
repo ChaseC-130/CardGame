2d57bf6bd0d03849e263f1e9ce61b6240614c248
Chase Cargill <chasecargill@Chases-MacBook-Air.local>
2020-08-03 19:37:50 -0500

    Player and Game State updatse

--- src/Boar.java
+++ src/Boar.java
@@ -1,5 +1,4 @@
 
-
 public class Boar extends Card {
 
 	Boar(String name, int maxHealth, int maxAttack) {
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,32 @@
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Main {
+	
+
+	public static LinkedList<Player> list = new LinkedList();
+
 
 	public static void main(String[] args) {
 		
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Number of Players");
+		int numPlayers = input.nextInt();
+		System.out.println("Choose a Hero: ");
+		System.out.println("1) Tank");
+		System.out.println("2) Damage");
+		
+		
+		
+		int choice = Integer.parseInt(input.nextLine());
+		
+		for (int i = 1; i <= numPlayers; i++) {
+			Player player = new Player("Player " + i, 20, 5);
+			list.add(player);
+		}
+		
+		
+		
 	}
 
 }
